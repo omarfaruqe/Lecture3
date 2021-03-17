03709105ff0e2ca8921c3a5a58d06576df93d754
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-17 23:17:46 +0600

    If Statements

---
+++ Lecture3.java
@@ -0,0 +1,9 @@
+public class Lecture3{
+	public static void main(String[] args) {
+		boolean isCold = true; // false;
+		if(isCold){
+			//executes ONLY if isCold is true
+			System.out.println("It's cold, wear a jacket!");
+		}
+	}
+}

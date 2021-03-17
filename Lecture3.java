ad881503dc946169c8ddca10fe5624a5453411bc
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-17 23:40:05 +0600

    Variable Scope

--- Lecture3.java
+++ Lecture3.java
@@ -1,26 +1,35 @@
 public class Lecture3{
 	public static void main(String[] args) {
-		boolean isCold = true; // false;
-		if(isCold){
-			//executes ONLY if isCold is true
-			System.out.println("It's cold, wear a jacket!");
-		}
+		// boolean isCold = true; // false;
+		// if(isCold){
+		// 	//executes ONLY if isCold is true
+		// 	System.out.println("It's cold, wear a jacket!");
+		// }
 
-		boolean isLightGreen = true; // false;
-		if(isLightGreen){
-			//traffic light is green
-			System.out.println("Drive!");
-		}
+		// boolean isLightGreen = true; // false;
+		// if(isLightGreen){
+		// 	//traffic light is green
+		// 	System.out.println("Drive!");
+		// }
 
 		/*Given the boolean variable isRaining ,
 		  write an if statement that will print out
 		  Windsheild wipers on.
 		  when its raining.
 		*/
-		boolean isRaining = true; // false;
-		if(isRaining){
+		// boolean isRaining = true; // false;
+		// if(isRaining){
+		// 	//traffic light is green
+		// 	System.out.println("Windsheild wipers on.");
+		// }
+
+		boolean isLightGreen = true; // false;
+		if(isLightGreen){
 			//traffic light is green
-			System.out.println("Windsheild wipers on.");
+			double carSpeed = 100; // in km/hr
+			System.out.println("Drive!");
+			System.out.println("Speed is: " + carSpeed);
 		}
+		carSpeed = carSpeed - 10;  // This will cause an error because its outside
 	}
 }
